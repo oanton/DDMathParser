b68bbfabab33a7493ef9daeb6528372a0e76d2ed
Anton Nebylytsia <anton.nebylytsia@gmail.com>
2022-12-01 03:50:24 +0200

    clone objc from https://github.com/davedelong/DDMathParser

---
+++ Unit Tests/ParserTests.h
@@ -0,0 +1,16 @@
+//
+//  ParserTests.h
+//  DDMathParser
+//
+//  Created by Dave DeLong on 11/11/10.
+//  Copyright 2010 Home. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+
+@interface ParserTests : XCTestCase {
+
+}
+
+@end
---
+++ Unit Tests/TokenizerTests.h
@@ -0,0 +1,19 @@
+//
+//  TokenizerTests.h
+//  DDMathParser
+//
+//  Created by Dave DeLong on 11/16/10.
+//  Copyright 2010 Home. All rights reserved.
+//
+//  See Also: http://developer.apple.com/iphone/library/documentation/Xcode/Conceptual/iphone_development/135-Unit_Testing_Applications/unit_testing_applications.html
+
+#import <XCTest/XCTest.h>
+
+
+@interface TokenizerTests : XCTestCase {
+
+}
+
+- (void)testTokenizer;
+
+@end
